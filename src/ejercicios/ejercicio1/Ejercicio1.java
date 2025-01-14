b287600c54bc55ea48d7b5e56e204191515dce69
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-01-14 13:01:09 +0100

    Enunciado ejercicio 1 en rama master

--- src/ejercicios/ejercicio1/Ejercicio1.java
+++ src/ejercicios/ejercicio1/Ejercicio1.java
@@ -2,10 +2,16 @@ package ejercicios.ejercicio1;
 
 /**
  * 1. Haz un programa que dentro de un hilo muestre: nombre del hilo, su prioridad, y los hilos activos.
- *
+ * <p>
  * Lanza al menos 5 hilos con diferentes nombres y prioridades.
+ * <p>
+ * Añade lo necesario para que no empiece a ejecutarse un hilo hasta que no haya finalizado el anterior.
  */
 
 public class Ejercicio1 {
+
+}
+
+class Main {
 
 }
