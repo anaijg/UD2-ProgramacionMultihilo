b61d3f4a72167a65b88e8ef526ebc36ce00e3fbe
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-01-14 12:30:35 +0100

    Enunciado ejercicio 1

---
+++ src/ejercicios/ejercicio1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ejercicios.ejercicio1;
+
+/**
+ * 1. Haz un programa que dentro de un hilo muestre: nombre del hilo, su prioridad, y los hilos activos.
+ *
+ * Lanza al menos 5 hilos con diferentes nombres y prioridades.
+ */
+
+public class Ejercicio1 {
+
+}
