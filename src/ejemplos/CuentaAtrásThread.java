9210bf642e83eb5612202f86b88ce7dc1284a7c2
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-01-17 09:44:31 +0100

    merged CuantaAtrásThread.java into master

--- src/ejemplos/CuentaAtrásThread.java
+++ src/ejemplos/CuentaAtrásThread.java
@@ -1,23 +1,22 @@
 package ejemplos;
 
 public class CuentaAtrásThread extends Thread{
-    private int countDown = 10;
-    private static int taskCount = 0;
-
-    public CuentaAtrásThread() {
-        taskCount++;
-    }
+    private int countDown;
+    private int taskNumber;
+    private static int taskCounter = 0;
 
     public CuentaAtrásThread(int countDown) {
         this.countDown = countDown;
-        taskCount++;
+        taskCounter++;
+        this.taskNumber = taskCounter;
+
     }
 
     @Override
     public void run() {
         // aquí metemos lo que tenga que hacer el hilo, en este caso contar hacia atrás
         while (countDown > 0) {
-            System.out.println("#" + taskCount + " (" + countDown + ")");
+            System.out.println("#" + taskNumber + " (" + countDown + ")");
             countDown--;
         }
     }
@@ -25,12 +24,12 @@ public class CuentaAtrásThread extends Thread{
     public static void main(String[] args) {
         // primer hilo
         System.out.println("¡Comienza la cuenta atrás!");
-        CuentaAtrásThread cuentaAtrás1 = new CuentaAtrásThread();
+        CuentaAtrásThread cuentaAtrás1 = new CuentaAtrásThread(10);
         cuentaAtrás1.start();
 
         // segundo hilo
         System.out.println("¡Comienza la cuenta atrás!");
-        CuentaAtrásThread cuentaAtrás2 = new CuentaAtrásThread();
+        CuentaAtrásThread cuentaAtrás2 = new CuentaAtrásThread(5);
         cuentaAtrás2.start();
     }
 }
