6115d95fd5c6d216883a2ab832fc295ff4db8aa7
MeLLamoTawet <g4ou7d@hotmail.com>
2025-01-17 18:30:23 +0100

    Cambios

--- src/ejercicios/ejercicio1/Ejercicio1_Erik.java
+++ src/ejercicios/ejercicio1/Ejercicio1_Erik.java
@@ -8,10 +8,22 @@ package ejercicios.ejercicio1;
  * Añade lo necesario para que no empiece a ejecutarse un hilo hasta que no haya finalizado el anterior.
  */
 
-public class Ejercicio1_Erik {
+public class Ejercicio1_Erik implements Runnable {
 
+    @Override
+    public void run() {
+        System.out.printf("Hilo corriendo: %s\n", Thread.currentThread().getName());
+    }
 }
 
 class Main {
-
+    public static void main(String[] args) {
+        Ejercicio1_Erik task = new Ejercicio1_Erik();
+        Thread hilo1 = new Thread(task);
+        Thread hilo2 = new Thread(task, "Hilo negro");
+        hilo1.start();
+        hilo2.start();
+        System.out.printf("Hilo hilo1: %s\n", hilo1.getState());
+        System.out.printf("El Main es este: %s\n", Thread.currentThread().getName());
+    }
 }
