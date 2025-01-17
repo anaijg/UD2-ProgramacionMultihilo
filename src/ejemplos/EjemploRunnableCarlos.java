d82d8112d871e24828bf4eac97c6fcdd14d89314
carlosDAM2905 <carlosdam2905@gmail.com>
2025-01-17 18:34:43 +0100

    Ejemplo runnable carlos

---
+++ src/ejemplos/EjemploRunnableCarlos.java
@@ -0,0 +1,35 @@
+package ejemplos;
+
+/**
+ * dos formas de crear hilos:
+ * 1) Implementando Runnable <- esta es la (recomendada)
+ * 2) Extendiendo Thread
+ */
+public class EjemploRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        // aquí dentro ponemos el código que quieres que ejecute el hilo
+        System.out.println("Hilo corriendo " + Thread.currentThread().getName());
+    }
+}
+
+// creo la clase donde se ejecutan los hilos
+class Main {
+    public static void main(String[] args) {
+        // Para lanzar un hilo:
+        // 1) Creo un objeto de la clase que implementa Runnable
+        EjemploRunnable task1 = new EjemploRunnable();
+        // 2) Ahora si, creamos el objeto Thread pasándole en el constructor la tarea "task"
+        Thread hilo1 = new Thread(task1);
+        Thread hilo2 = new Thread(task1, "hilo negro");
+        // 3) Lanzamos el hilo con el método start()
+        hilo1.start();
+        hilo2.start();
+
+        // Y siempre anda por detrás el hilo main
+        System.out.println("El hilo principal es este: " + Thread.currentThread().getName());
+    }
+}
+
+
