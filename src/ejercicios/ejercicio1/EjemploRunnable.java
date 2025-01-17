5946cc471e39593fae99eaf30ce3d9586c398f3d
Kamilly-cod <kamillybritodm@gmail.com>
2025-01-17 18:34:58 +0100

    kamilly modificaciones

---
+++ src/ejercicios/ejercicio1/EjemploRunnable.java
@@ -0,0 +1,23 @@
+package ejercicios.ejercicio1;
+
+public class EjemploRunnable implements Runnable{
+@Override
+    public void run(){
+    System.out.println("Hilo corriente " +Thread.currentThread().getName());
+}
+}
+
+class Main {
+    public static void main(String[] args) {
+        EjemploRunnable task1 = new EjemploRunnable();
+        // 2) Ahora si, creamos el objeto Thread pasándole en el constructor la "task"
+        Thread hilo1 = new Thread(task1);
+        Thread hilo2 = new Thread(task1,"Hilo negro");
+        // 3) Lanzamos el hilo con el método start()
+        hilo1.start();
+        hilo2.start();
+
+        // y siempre anda por detrás el hilo main
+        System.out.println("El hilo principal es este: " +Thread.currentThread().getName());
+    }
+}
--- src/ejercicios/ejercicio1/Ejercicio1_Kamilly.java
+++ src/ejercicios/ejercicio1/Ejercicio1_Kamilly.java
@@ -1,17 +1,7 @@
 package ejercicios.ejercicio1;
 
-/**
- * 1. Haz un programa que dentro de un hilo muestre: nombre del hilo, su prioridad, y los hilos activos.
- * <p>
- * Lanza al menos 5 hilos con diferentes nombres y prioridades.
- * <p>
- * Añade lo necesario para que no empiece a ejecutarse un hilo hasta que no haya finalizado el anterior.
- */
+class Ejercicio1_Kamilly {
+    public static void main(String[] args) {
 
-public class Ejercicio1_Aina {
-
-}
-
-class Main {
-
+    }
 }
