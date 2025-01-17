9c45b5c1e0bf68a47ebb1a12353adcf24ab73911
SISA0729 <ismael.sisa@educa.madrid.org>
2025-01-17 18:56:53 +0100

    sisa

--- src/ejercicios/ejemplo/EjemploRunnableSisa.java
+++ src/ejercicios/ejemplo/EjemploRunnableSisa.java
@@ -7,17 +7,17 @@ public class EjemploRunnableSisa implements Runnable{
         System.out.println("Hola, soy un hilo" + Thread.currentThread().getName());
     }
 
-    class Main{
-        public static void main(String[] args) {
-            EjemploRunnableSisa task1 = new EjemploRunnableSisa();
-            Thread hilo = new Thread(task1);
-            Thread hilo1 = new Thread(task1, "Hilo 1");
+}
+class Main{
+    public static void main(String[] args) {
+        EjemploRunnableSisa task1 = new EjemploRunnableSisa();
+        Thread hilo = new Thread(task1);
+        Thread hilo1 = new Thread(task1, "Hilo 1");
 
-            hilo.start();
+        hilo.start();
 
-            System.out.println("El hilo principal es este: " +
-                    Thread.currentThread().getName());
+        System.out.println("El hilo principal es este: " +
+                Thread.currentThread().getName());
 
-        }
     }
 }
