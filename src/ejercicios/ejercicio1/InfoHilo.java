25dd77709a23ced4d6642640638d8e8d0c5155f3
carlosDAM2905 <carlosdam2905@gmail.com>
2025-01-21 19:25:01 +0100

    ejercicio multihilo(revisar en casa)

--- src/ejercicios/ejercicio1/InfoHilo.java
+++ src/ejercicios/ejercicio1/InfoHilo.java
@@ -3,38 +3,47 @@ package ejercicios.ejercicio1;
 public class InfoHilo implements Runnable {
     @Override
     public void run() {
-        // Obtenemos el hilo actual
+        // Obtenemos el hilo actual; se refiere al hilo que estamos ejecutando a partir de la clase
+        Thread hiloActual = Thread.currentThread();
+
 
         // Mostramos la información del hilo actual
         System.out.println("=== Información del Hilo Actual ===");
-        System.out.println("Nombre del hilo: ");
-        System.out.println("Prioridad del hilo: ");
-        System.out.println("ID del hilo: ");
+        System.out.println("Nombre del hilo: " + hiloActual.getName());
+        System.out.println("Prioridad del hilo: " + hiloActual.getPriority()); // mayor prioridad: 10 , menor prioridad: 1
+        System.out.println("ID del hilo: " + hiloActual.getId());
 
 
         // Contamos el número de hilos activos
-        //int hilosActivos =
-        //Thread[] arrayHilosActivos = ;
+        int hilosActivos = hiloActual.getThreadGroup().activeCount();
+        Thread[] arrayHilosActivos = new Thread[hilosActivos];
+
 
         // Obtenemos todos los hilos activos
-        //int hilosReales = Thread.currentThread().getThreadGroup().enumerate(arrayHilosActivos, true);
+        int hilosReales = Thread.currentThread().getThreadGroup().enumerate(arrayHilosActivos, true);
 
 
         // Mostramos el número total de hilos activos y sus nombres
         System.out.println("\n=== Hilos Activos ===");
+        for (int i = 0; i < hilosReales; i++) {
+            System.out.println((i + 1)+ " " + arrayHilosActivos[i].getName());
+        }
 
     }
 
     public static void main(String[] args) {
+        System.out.println("En el main imprimo Thread.currentThread: "+ Thread.currentThread().getName());
         // Creamos una instancia de nuestra clase
-
+        // 1) creamos un objeto "tipo" Runnable
+        InfoHilo task1 = new InfoHilo();
         // Creamos un nuevo hilo con un nombre personalizado
-
+        //2) Creamos un hilo y le pasamos en el constructor la task
+        Thread hilo1 = new Thread(task1, "Mi hilo favorito");
         // Establecemos una prioridad personalizada (1-10)
-
+        hilo1.setPriority(8);
         // Iniciamos el hilo
+        hilo1.start();
 
-        // Esperamos a que el hilo termine
 
     }
 }
