feb752cf4b07fc107a6017511a4960f5428f448c
johan <johanencisoholguin@gmil.com>
2025-01-21 18:58:58 +0100

    Adios Aina
    eliminando Aina

--- src/ejercicios/ejercicio1/InfoHilo.java
+++ src/ejercicios/ejercicio1/InfoHilo.java
@@ -4,9 +4,9 @@ public class InfoHilo implements Runnable {
     @Override
     public void run() {
         // Obtenemos el hilo actual
-
+        Thread hiloActual = Thread.currentThread();
         // Mostramos la información del hilo actual
-        System.out.println("=== Información del Hilo Actual ===");
+        System.out.println("\n === Información del Hilo Actual === \n");
         System.out.println("Nombre del hilo: ");
         System.out.println("Prioridad del hilo: ");
         System.out.println("ID del hilo: ");
@@ -27,14 +27,18 @@ public class InfoHilo implements Runnable {
 
     public static void main(String[] args) {
         // Creamos una instancia de nuestra clase
-
+        System.out.println(Thread.currentThread());
         // Creamos un nuevo hilo con un nombre personalizado
-
+         InfoHilo task1 = new InfoHilo();
         // Establecemos una prioridad personalizada (1-10)
-
+        Thread hilo1 = new Thread(task1, "Hilo 1");
         // Iniciamos el hilo
-
+        hilo1.start();
         // Esperamos a que el hilo termine
-
+        try {
+            hilo1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
