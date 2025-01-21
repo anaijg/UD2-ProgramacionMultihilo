19054a55bc7d9b0f60646a0b88313e43982de18e
Kamilly-cod <kamillybritodm@gmail.com>
2025-01-21 18:06:03 +0100

    teste

---
+++ src/ejemplos/EjemploThread.java
@@ -0,0 +1,22 @@
+package ejemplos;
+
+public class EjemploThread extends Thread {
+
+    @Override
+    public void run(){
+        System.out.println("Hilo corriente " +Thread.currentThread().getName());
+    }
+}
+
+class Main2 {
+    public static void main(String[] args) {
+        EjemploThread hilo1 = new EjemploThread();
+        hilo1.start();
+
+        EjemploThread hilo2 = new EjemploThread();
+        hilo2.start();
+
+        System.out.println("Hilo principal: " +Thread.currentThread().getName());
+
+    }
+}
