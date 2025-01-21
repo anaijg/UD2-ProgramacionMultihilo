ef12fbd0d7e171ef0ce5c543f0a8609a7cabbdc5
juanda1612 <juan.perez77@educa.madrid.com>
2025-01-21 19:07:38 +0100

    ss

--- src/ejercicios/ejercicio1/InfoHiloJuanDaniel.java
+++ src/ejercicios/ejercicio1/InfoHiloJuanDaniel.java
@@ -1,9 +1,13 @@
 package ejercicios.ejercicio1;
 
-public class InfoHilo implements Runnable {
+import javax.swing.plaf.TableHeaderUI;
+
+public class InfoHiloJuanDaniel implements Runnable {
     @Override
     public void run() {
         // Obtenemos el hilo actual
+        // aqui se refiere al hilo que estamos ejecutando a partir de la clase InfoHiloJuanDaniel
+        Thread hiloActual = Thread.currentThread();
 
         // Mostramos la información del hilo actual
         System.out.println("=== Información del Hilo Actual ===");
@@ -26,9 +30,15 @@ public class InfoHilo implements Runnable {
     }
 
     public static void main(String[] args) {
+        System.out.println("En el main imprimo Thread.currentThread: " + Thread.currentThread().getName());
         // Creamos una instancia de nuestra clase
+        // 1) primero creamos un objeto de tipo Runnable
+        InfoHiloJuanDaniel task1 = new InfoHiloJuanDaniel();
 
         // Creamos un nuevo hilo con un nombre personalizado
+        // Creamos un hilo y le pasamos en el contructor la task
+        Thread hilo1 = new Thread(task1, "Mi hilo favorito");
+        hilo1.start();
 
         // Establecemos una prioridad personalizada (1-10)
 
