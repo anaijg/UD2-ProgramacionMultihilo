28bef1a64fce33df2207b90fe4b099b3e09bb9d8
alcarazaina <alcarazdainara@gmail.com>
2025-01-21 18:58:42 +0100

    ...

--- src/ejercicios/ejercicio1/InfoHilo_Aina.java
+++ src/ejercicios/ejercicio1/InfoHilo_Aina.java
@@ -1,9 +1,12 @@
 package ejercicios.ejercicio1;
 
-public class InfoHilo implements Runnable {
+public class InfoHilo_Aina implements Runnable {
     @Override
     public void run() {
         // Obtenemos el hilo actual
+        //Aquí se refiere al hilo que estamos ejecutando a partir de la clase
+
+        Thread hiloActual = Thread.currentThread();
 
         // Mostramos la información del hilo actual
         System.out.println("=== Información del Hilo Actual ===");
@@ -26,9 +29,18 @@ public class InfoHilo implements Runnable {
     }
 
     public static void main(String[] args) {
+
+        //Aquí nos referimos al hilo de main
+        System.out.println("En el main imprimo Thread.currentThread -> " + Thread.currentThread().getName());
+
         // Creamos una instancia de nuestra clase
+        // 1) Creamos un objeto de la clase que implementa Runable
+        InfoHilo_Aina task1 = new InfoHilo_Aina();
 
         // Creamos un nuevo hilo con un nombre personalizado
+        // Creamos un hilo y le pasamos en el constructor la task
+        Thread hilo1 = new Thread(task1, "Mi hilo favorito");
+        hilo1.start();
 
         // Establecemos una prioridad personalizada (1-10)
 
