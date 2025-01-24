3e776ba47a5a19dee7c12df3fbe0a36074284d98
Kamilly-cod <kamillybritodm@gmail.com>
2025-01-24 17:42:27 +0100

    info

--- src/ejercicios/ejercicio1/InfoHilo.java
+++ src/ejercicios/ejercicio1/InfoHilo.java
@@ -10,21 +10,30 @@ public class InfoHilo implements Runnable {
 
         // Mostramos la información del hilo actual
         System.out.println("=== Información del Hilo Actual ===");
-        System.out.println("Nombre del hilo: ");
-        System.out.println("Prioridad del hilo: ");
-        System.out.println("ID del hilo: ");
+        System.out.println("Nombre del hilo: "+hilo_actual.getName());
+        System.out.println("Prioridad del hilo: "+hilo_actual.getPriority());
+        System.out.println("ID del hilo: "+hilo_actual.getId());
 
 
         // Contamos el número de hilos activos
-        //int hilosActivos =
+        int hiloActivos = hilo_actual.getThreadGroup().activeCount();
+
         //Thread[] arrayHilosActivos = ;
+        Thread[] arrayHilosActivos = new Thread[hiloActivos];
+        for (int i = 0; i < hiloActivos ; i++) {
+
+        }
 
         // Obtenemos todos los hilos activos
-        //int hilosReales = Thread.currentThread().getThreadGroup().enumerate(arrayHilosActivos, true);
+        System.out.println("\n== Hilos Activos ===");
+        int hilosReales = Thread.currentThread().getThreadGroup().enumerate(arrayHilosActivos, true);
 
 
         // Mostramos el número total de hilos activos y sus nombres
         System.out.println("\n=== Hilos Activos ===");
+        for (int i = 0; i < hilosReales ; i++) {
+            System.out.println((i+1) +""+arrayHilosActivos[i].getName());
+        }
 
     }
 
@@ -35,12 +44,14 @@ public class InfoHilo implements Runnable {
         // Creamos un nuevo hilo con un nombre personalizado
         //2) Creamos un nuevo hilo con un nombre personalizado
         Thread hilo1 = new Thread(task1,"Mi hilo favorito");
-        hilo1.start();
+
         // Establecemos una prioridad personalizada (1-10)
+        hilo1.setPriority(8);
 
         // Iniciamos el hilo
-
+        hilo1.start();
         // Esperamos a que el hilo termine
+
 
     }
 }
