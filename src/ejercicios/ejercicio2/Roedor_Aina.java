d07260f3ebbbd3383249a2873502023c004a8545
alcarazaina <alcarazdainara@gmail.com>
2025-01-24 18:02:40 +0100

    aaaaaah

--- src/ejercicios/ejercicio2/Roedor_Aina.java
+++ src/ejercicios/ejercicio2/Roedor_Aina.java
@@ -17,12 +17,35 @@ public class Roedor_Aina implements Runnable{
     }
 
     public void comer(){
-        System.out.println("Color: " + color + "\nNombre: " + nombre + "\n Emoji: " + emoji);
-        System.out.println(nombre + " Ha terminado de comer en " + tiempoEnComer + " segundos");
+        try {
+            System.out.println(color.getCode() + nombre + emoji.getEmoji() + " empieza a comer");
+            Thread.sleep(tiempoEnComer * (long) 1000);
+            System.out.println(color.getCode() + nombre + " Ha terminado de comer en " + tiempoEnComer + " segundos" + emoji.getEmoji());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
     }
 
     @Override
     public void run() {
         comer();
+    }
+
+    public static void main(String[] args) {
+        Roedor_Aina Fievel = new Roedor_Aina("Fievel", 4, Color.BLACK, Emoji.RAT);
+        Roedor_Aina Jerry = new Roedor_Aina("Jerry", 5, Color.RED, Emoji.CHIPMUNK);
+        Roedor_Aina Pinky = new Roedor_Aina("Pinky", 3, Color.PURPLE, Emoji.MOUSE);
+        Roedor_Aina Mickey = new Roedor_Aina("Mickey", 6, Color.WHITE, Emoji.HAMSTER);
+
+        Thread hiloF = new Thread(Fievel);
+        Thread hiloJ = new Thread(Jerry);
+        Thread hiloP = new Thread(Pinky);
+        Thread hiloM = new Thread(Mickey);
+
+        hiloF.start();
+        hiloJ.start();
+        hiloP.start();
+        hiloM.start();
     }
 }
