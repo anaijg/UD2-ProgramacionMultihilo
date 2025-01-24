f4bc17ba1d2fd6a777f1a2e927c0f2dbb9d46e56
johan <johanencisoholguin@gmil.com>
2025-01-24 17:39:28 +0100

    dios los crea y ellos se juntan

--- src/ejercicios/ejercicio1/InfoHilo.java
+++ src/ejercicios/ejercicio1/InfoHilo.java
@@ -7,14 +7,18 @@ public class InfoHilo implements Runnable {
         Thread hiloActual = Thread.currentThread();
         // Mostramos la información del hilo actual
         System.out.println("\n === Información del Hilo Actual === \n");
-        System.out.println("Nombre del hilo: ");
-        System.out.println("Prioridad del hilo: ");
-        System.out.println("ID del hilo: ");
+        System.out.println("Nombre del hilo: " + hiloActual.getName());
+        System.out.println("Prioridad del hilo: " + hiloActual.getPriority());
+        System.out.println("ID del hilo: " + hiloActual.getId());
 
-
+        int hilosActivos = hiloActual.getThreadGroup().activeCount();
+        Thread[] arrayHilosActivos = new Thread[hilosActivos];
         // Contamos el número de hilos activos
-        //int hilosActivos =
-        //Thread[] arrayHilosActivos = ;
+        System.out.printf("\nNúmero de hilos activos: %d\n", hilosActivos);
+        int hilosReales = Thread.currentThread().getThreadGroup().enumerate(arrayHilosActivos, true);
+        for (int i = 0; i < hilosReales; i++) {
+            System.out.println((i + 1) + ". " + arrayHilosActivos[i].getName());
+        }
 
         // Obtenemos todos los hilos activos
         //int hilosReales = Thread.currentThread().getThreadGroup().enumerate(arrayHilosActivos, true);
@@ -32,10 +36,21 @@ public class InfoHilo implements Runnable {
          InfoHilo task1 = new InfoHilo();
         // Establecemos una prioridad personalizada (1-10)
         Thread hilo1 = new Thread(task1, "Hilo 1");
-        // Iniciamos el hilo
+        // La línea hilo1.setPriority(10); establece la prioridad del hilo hilo1 al
+        // valor máximo permitido, que es 10. En Java, los hilos pueden tener una
+        // prioridad entre 1 (mínima) y 10 (máxima). La prioridad de un hilo puede
+        // influir en el orden en que los hilos son programados para ejecución por
+        // el sistema operativo, aunque no garantiza un orden específico.
+        hilo1.setPriority(10);
+        // Iniciamos el hilo con el metodo start()
         hilo1.start();
         // Esperamos a que el hilo termine
         try {
+            // La línea hilo1.join(); en el código de Java hace que el hilo actual
+            // espere a que el hilo hilo1 termine su ejecución antes de continuar.
+            // Esto es útil cuando necesitas asegurarte de que un hilo específico haya
+            // completado su tarea antes de proceder con la ejecución del código en el
+            // hilo principal o en otro hilo.
             hilo1.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
