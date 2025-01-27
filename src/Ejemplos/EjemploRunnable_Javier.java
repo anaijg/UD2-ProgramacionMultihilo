e00d81dc8821100c402f9d175924996eca535569
Javialj <javialj12@gmail.com>
2025-01-27 20:20:05 +0100

    27/01/25

--- src/Ejemplos/EjemploRunnable_Javier.java
+++ src/Ejemplos/EjemploRunnable_Javier.java
@@ -11,7 +11,12 @@ public class EjemploRunnable_Javier implements Runnable {
 class Main {
     public static void main(String[] args) {
         EjemploRunnable_Javier task1 = new EjemploRunnable_Javier();
+        Thread hilo1 = new Thread(task1);
+        Thread hilo2 = new Thread(task1, "Hilo negro");
 
+        hilo1.start();
+        hilo2.start();
 
+        System.out.println("El hilo principal es este: " + Thread.currentThread().getName());
     }
 }
