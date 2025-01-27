b698da7f3279919d71bb1984ac4d4797a46e6a02
David de la Torre <80260076+DavidDeLaTorre0@users.noreply.github.com>
2025-01-27 14:19:34 +0100

    Add files via upload

--- src/ejercicios/ejercicio3/Roedor.java
+++ src/ejercicios/ejercicio3/Roedor.java
@@ -1,26 +1,22 @@
-package ejercicios.ejercicio2;
+package ejercicios.ejercicio3;
 
+import ejercicios.ejercicio3.entity.RoedorEntity;
 import utilidades.Color;
 import utilidades.Emoji;
 
 public class Roedor implements Runnable{
-    private String nombre;
-    private int tiempoEnComer;
-    private Color color;
-    private Emoji emoji;
+    private final RoedorEntity roe ;
 
     public Roedor(String nombre, int tiempoEnComer, Color color, Emoji emoji) {
-        this.nombre = nombre;
-        this.tiempoEnComer = tiempoEnComer;
-        this.color = color;
-        this.emoji = emoji;
+        this.roe = new RoedorEntity(nombre, tiempoEnComer, color, emoji);
     }
+
 
     public void comer(){
         try {
-            System.out.println(color.getCode() + "El ratón " + nombre + emoji.getEmoji() + " ha empezado a alimentarse.");
-            Thread.sleep(tiempoEnComer * (long) 1000);
-            System.out.println(color.getCode() + "El ratón " + nombre + emoji.getEmoji() + " ha terminado de alimentarse.");
+            System.out.println(roe.getColor() + "El ratón " + roe.getNombre() + roe.getEmoji() + " ha empezado a alimentarse.");
+            Thread.sleep(roe.getTiempoEnComer() * (long) 1000);
+            System.out.println(roe.getColor() + "El ratón " + roe.getNombre() + roe.getEmoji() + " ha terminado de alimentarse.");
         } catch (InterruptedException e) {
             System.out.println("Interrupted exception del método sleep.");
             /* Clean up whatever needs to be handled before interrupting  */
@@ -30,7 +26,7 @@ public class Roedor implements Runnable{
 
     @Override
     public void run() {
-        this.comer();
+        comer();
     }
 }
 
