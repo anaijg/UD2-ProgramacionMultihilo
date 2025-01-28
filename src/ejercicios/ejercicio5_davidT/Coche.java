64293bdf143cc999b3a1f63fba394f0dc4bb9be7
David <datocam98@gmail.com>
2025-01-28 12:08:13 +0100

    avanzando con ej5

---
+++ src/ejercicios/ejercicio5_davidT/Coche.java
@@ -0,0 +1,37 @@
+package ejercicios.ejercicio5_davidT;
+
+import utilidades.Color;
+import utilidades.Emoji;
+
+public class Coche implements Runnable{
+
+    private String nombre;
+    private int velocidad;
+    private int distanciaCircuito;
+    private int totalRecorrido;
+
+    public Coche(String nombre, int velocidad, int distanciaCircuito) {
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.distanciaCircuito = distanciaCircuito;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    private void mostrarProgreso(){
+
+    }
+
+    private void avanzar(){
+
+    }
+}
+class CarreraCoches{
+    public static void main(String[] args) {
+
+    }
+
+}
