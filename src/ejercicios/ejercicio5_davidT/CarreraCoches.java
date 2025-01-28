dfdf353558c0cba7ecd4946f451d993a6add42c5
David <datocam98@gmail.com>
2025-01-28 12:41:50 +0100

    cambio, hecho como el diagrama, pero volviendome loco para sacar el progreso

---
+++ src/ejercicios/ejercicio5_davidT/CarreraCoches.java
@@ -0,0 +1,49 @@
+package ejercicios.ejercicio5_davidT;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CarreraCoches{
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int metrosCircui,numCoches,i,metrosSeg;
+
+        System.out.println("Introduce la distancia del circuito");
+        metrosCircui = sc.nextInt();
+        System.out.println("Introduce el numero de coches:(maximo 4)");
+        numCoches = sc.nextInt();
+
+        if(!(numCoches<1 || numCoches>4)){
+
+            ArrayList<Coche> coches = new ArrayList<>();
+            for(i=1;i<numCoches;i++){
+                System.out.println("Introduce la velocidad del coche "+i+ "metros/segundo:");
+                metrosSeg= sc.nextInt();
+                coches.add(new Coche("Coche"+i,metrosSeg,metrosCircui));
+            }
+            ArrayList<Thread> hilos = new ArrayList<>();
+            for(Coche coche: coches){
+                Thread hilo = new Thread(coche);
+                hilos.add(hilo);
+            }
+
+            System.out.println("Comienza la carrera");
+            //Iniciamos todos los hilos por igual
+            for (Thread hilo: hilos){
+                hilo.start();
+            }
+
+            for(Thread hilo: hilos){
+                try{
+                    hilo.join();
+                }catch (InterruptedException ex){
+                    System.out.println("Accidente en la pista" + ex.getMessage());
+                }
+            }
+
+        }else {
+            System.out.println("El numero de coches no es valido");
+        }
+    }
+
+}
--- src/ejercicios/ejercicio5_davidT/Coche.java
+++ src/ejercicios/ejercicio5_davidT/Coche.java
@@ -1,14 +1,12 @@
 package ejercicios.ejercicio5_davidT;
 
-import utilidades.Color;
-import utilidades.Emoji;
 
 public class Coche implements Runnable{
 
     private String nombre;
-    private int velocidad;
-    private int distanciaCircuito;
-    private int totalRecorrido;
+    private int velocidad,distanciaCircuito,totalRecorrido,progreso;
+    //Necesito tener algo que me diga si he finalizado
+    private boolean fin = false;
 
     public Coche(String nombre, int velocidad, int distanciaCircuito) {
         this.nombre = nombre;
@@ -19,6 +17,13 @@ public class Coche implements Runnable{
     @Override
     public void run() {
 
+        while(progreso<distanciaCircuito && !fin){
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+
+            }
+        }
     }
 
     private void mostrarProgreso(){
@@ -28,10 +33,4 @@ public class Coche implements Runnable{
     private void avanzar(){
 
     }
-}
-class CarreraCoches{
-    public static void main(String[] args) {
-
-    }
-
 }
