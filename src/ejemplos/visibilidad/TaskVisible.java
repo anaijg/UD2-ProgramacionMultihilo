98e0a15f9fc9f3a6b7adc9bedde76014f7e9bd91
Daniel <danielhdezgarcia9daw@gmail.com>
2025-01-31 17:58:23 +0100

    Merged src/ from master to danielhdez

---
+++ src/ejemplos/visibilidad/TaskVisible.java
@@ -0,0 +1,8 @@
+package ejemplos.visibilidad;
+
+import java.util.Random;
+
+/**
+ * Clase TaskVisible
+ * Implementa run(), donde se crea un ejemploVisibilidad y se incrementa en cinco y muestra el nombre del hilo y el valor resultante.
+ */
