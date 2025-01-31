e6d21f1a172bbe479244e97aeaa0703765314a64
alcarazaina <alcarazdainara@gmail.com>
2025-01-31 17:50:26 +0100

    teoria y ejemplos

---
+++ src/ejemplos/memoriacompartida/HiloContador.java
@@ -0,0 +1,9 @@
+package ejemplos.memoriacompartida;
+
+/**
+ * Clase Hilocontador, que hereda de Thread
+ * tiene una constante de tipo Contador que se inicializa con un Contador pasado en el constructor.
+ * Sobreescribe el méto-do run() llamando al método incrementar del contador.
+ * NOTA: hacer también este ejemplo con Runnable
+ */
+
