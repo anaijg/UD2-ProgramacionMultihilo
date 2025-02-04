989a4f6d6d6813e1454efa6c40b0df878041e30e
David <datocam98@gmail.com>
2025-02-04 10:29:32 +0100

    Contador hecho

--- src/ejemplos/memoriacompartida/Contador.java
+++ src/ejemplos/memoriacompartida/Contador.java
@@ -5,3 +5,19 @@ package ejemplos.memoriacompartida;
  * - atributo privado entero valor, con un getter
  * - método de instancia incrementar(), que incrementa valor en una unidad
  */
+
+class Contador {
+    private int contador;
+
+    public Contador(int contador) {
+        this.contador = contador;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public int incrementar(){
+        return this.contador++;
+    }
+}
