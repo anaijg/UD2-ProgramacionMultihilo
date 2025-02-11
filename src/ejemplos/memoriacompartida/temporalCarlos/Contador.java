865f23b6138826c046e8e08e71ee506ada731236
Carlos <146743981+carlosDAM2905@users.noreply.github.com>
2025-02-11 18:51:16 +0100

    temporalCarlos (#24)
    * reorganización ejercicios
    
    * ejercicio contador Carlos

---
+++ src/ejemplos/memoriacompartida/temporalCarlos/Contador.java
@@ -0,0 +1,21 @@
+package ejemplos.memoriacompartida.temporalCarlos;
+
+/**
+ * Clase contador:
+ * - atributo privado entero valor, con un getter
+ * - método de instancia incrementar(), que incrementa valor en una unidad
+ */
+class Contador {
+    private int value = 0;
+
+    public void increment() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+}
+
