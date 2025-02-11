ba25f11513434562c66cb1850048f9cfdf9c4128
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-02-11 18:06:03 +0100

    ejemplo contador

--- src/ejemplos/memoriacompartida/MainContador.java
+++ src/ejemplos/memoriacompartida/MainContador.java
@@ -9,3 +9,23 @@ package ejemplos.memoriacompartida;
  * - Si alguno falla, se muestra el mensaje "Algo ha ido mal en algún join()". A continuación, se interrumpen los hilos que queden vivos.
  * - Finalmente, tanto si salta la excepción como si no, se muestra el valor del contador.
  */
+
+public class MainContador {
+    public static void main(String[] args) {
+        Contador contador = new Contador();
+        HiloContador hilo1 = new HiloContador(contador);
+        HiloContador hilo2 = new HiloContador(contador);
+        hilo1.start();
+        hilo2.start();
+
+        try {
+            hilo1.join();
+            hilo2.join();
+        } catch (InterruptedException e){
+            System.out.println("Vamos mal");
+            Thread.currentThread().interrupt();
+        } finally {
+            System.out.println("Valor final del contador: " + contador.getValor());
+        }
+    }
+}
